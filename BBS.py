2dcdb2d1e91329be466fd4288d786af3c8d3d4b2
buggs-bun <mathieu.bunet28@icloud.com>
2026-02-11 09:16:50 +0100

    maj bbs -> autocorrelation
    Added functions to generate bits and compute autocorrelation.

--- BBS.py
+++ BBS.py
@@ -1,6 +1,8 @@
 from random import randint
 from math import gcd
 from itertools import product
+import numpy as np
+import matplotlib.pyplot as plt
 
 p = 1000003
 q =  2001911
@@ -20,3 +22,31 @@ def BBS_generator(p = 1000003, q =  2001911, M = p * q, seed = randint(2, 10), T
   return int(bits, 2)
 
 
+
+# --- Génère une grande séquence de bits ---
+def generate_bits(n_bits=5000):
+    x = BBS_generator(TAILLE_NB_GENERE=n_bits)
+    bits = bin(x)[2:].zfill(n_bits)   # forcer la taille
+    return np.array([int(b) for b in bits])
+
+# --- Autocorrélation ---
+def autocorrelation(x, max_lag=50):
+    n = len(x)
+    x = x - np.mean(x)
+    result = []
+    for lag in range(1, max_lag+1):
+        c = np.corrcoef(x[:-lag], x[lag:])[0,1]
+        result.append(c)
+    return result
+
+# --- Test ---
+bits = generate_bits(5000)
+ac = autocorrelation(bits, max_lag=40)
+
+# --- Affichage ---
+plt.stem(range(1, 41), ac)
+plt.axhline(0, linestyle='--')
+plt.title("Autocorrélation des bits BBS")
+plt.xlabel("Lag (retard)")
+plt.ylabel("Corrélation")
+plt.show()
